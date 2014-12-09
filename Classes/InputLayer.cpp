40af648e2489d6ae2705009aa8d0d2f2a13b6ad2
phicdy <phicdy@gmail.com>
2014-12-09 12:28:11 +0900

    Add InputLayer

---
+++ Classes/InputLayer.cpp
@@ -0,0 +1,11 @@
+/*
+ * InputLayer.cpp
+ *
+ *  Created on: 2014/11/25
+ *      Author: pluea
+ */
+
+#include "InputLayer.h"
+
+USING_NS_CC;
+
---
+++ Classes/InputLayer.h
@@ -0,0 +1,21 @@
+/*
+ * InputLayer.h
+ *
+ *  Created on: 2014/11/25
+ *      Author: pluea
+ */
+
+#ifndef INPUTLAYER_H_
+#define INPUTLAYER_H_
+
+#include "cocos2d.h"
+
+class InputLayer: public cocos2d::Layer {
+public:
+
+	CREATE_FUNC(InputLayer);
+};
+
+
+
+#endif
