caab7d405acee351af2394fba521f20f23b97e41
phicdy <phicdy@gmail.com>
2014-12-12 17:36:37 +0900

    Add BulletCache

---
+++ Classes/BulletCache.cpp
@@ -0,0 +1,3 @@
+#include "BulletCache.h"
+
+USING_NS_CC;
---
+++ Classes/BulletCache.h
@@ -0,0 +1,11 @@
+#ifndef BULLETCACHE_H_
+#define BULLETCACHE_H_
+
+#include "cocos2d.h"
+
+class BulletCache : public cocos2d::Node {
+public:
+	CREATE_FUNC(BulletCache);
+};
+
+#endif
