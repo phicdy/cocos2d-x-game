44f9c8fa00ac5d80e951007ebef93c9c3351572b
phicdy <phicdy@gmail.com>
2014-12-12 17:17:22 +0900

    Add StandardShootComponent

---
+++ Classes/StandardShootComponent.h
@@ -0,0 +1,11 @@
+#ifndef STANDARDSHOOTCOMPONENT_H_
+#define STANDARDSHOOTCOMPONENT_H_
+
+#include "cocos2d.h"
+
+class StandardShootComponent : public cocos2d::Sprite {
+public:
+	CREATE_FUNC(StandardShootComponent);
+};
+
+#endif
