37bde1ff6dcb0b3004722c7e46850e28dda562a2
pluea <kyamaguchi@Keisuke-no-Macbook-Pro.local>
2014-12-12 17:36:37 +0900

    Add BulletCache

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -22,6 +22,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/EnemyEntity.cpp \
                    ../../Classes/EnemyCache.cpp \
                    ../../Classes/StandardShootComponent.cpp \
+                   ../../Classes/BulletCache.cpp \
                    ../../Classes/Bullet.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
