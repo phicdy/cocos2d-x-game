2738511c92124ccfb662924583506d43088ca7aa
phicdy <phicdy@gmail.com>
2014-12-12 17:34:03 +0900

    Add init method and schedule update method

--- Classes/StandardShootComponent.cpp
+++ Classes/StandardShootComponent.cpp
@@ -1,3 +1,32 @@
 #include "StandardShootComponent.h"
+#include "GameScene.h"
 
 USING_NS_CC;
+
+bool StandardShootComponent::init() {
+	if (!Sprite::init()) {
+		return false;
+	}
+
+	this->scheduleUpdate();
+
+	return true;
+}
+
+void StandardShootComponent::update(float delta) {
+	Node *parent = this->getParent();
+	if (parent->isVisible()) {
+		updateCount++;
+
+		if (updateCount >= shootFrequency) {
+			//CCLOG(@"enemy %@ shoots!", self.parent);
+			updateCount = 0;
+
+			GameScene* game = GameScene::getSharedGameScene();
+			Point startPos = ccpSub(parent->getPosition(),
+					CCPointMake(parent->getContentSize().width * 0.5f, 0));
+//		[game.bulletCache shootBulletFrom:startPos velocity:CGPointMake(-2, 0) frameName:bulletFrameName];
+	}
+}
+
+}
