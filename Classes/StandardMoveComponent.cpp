c93c74269aeadb3a850dae9120e5a88c4f601fee
phicdy <phicdy@gmail.com>
2014-12-15 23:33:50 +0900

    Add init and schedule update method

--- Classes/StandardMoveComponent.cpp
+++ Classes/StandardMoveComponent.cpp
@@ -1,3 +1,25 @@
 #include "StandardMoveComponent.h"
+#include "Entity.h"
+#include "GameScene.h"
 
 USING_NS_CC;
+
+bool StandardMoveComponent::init() {
+	if (!Sprite::init()) {
+		return false;
+	}
+
+	velocity = CCPointMake(-1, 0);
+
+	this->scheduleUpdate();
+	return true;
+}
+
+void StandardMoveComponent::update(float delta) {
+	if (this->getParent()->isVisible()) {
+		Entity* entity = (Entity*)this->getParent();
+		if (entity->getPosition().x > GameScene::getSharedGameScene()->getContentSize().width * 0.5f) {
+			entity->setPosition(ccpAdd(entity->getPosition(), velocity));
+		}
+	}
+}
