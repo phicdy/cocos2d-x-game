b4d9f43ef43d30c247215715d18494e2094f2f93
pluea <pluea.ky@gmail.com>
2014-12-18 01:47:30 +0900

    Add DogEntity

---
+++ Classes/DogEntity.cpp
@@ -0,0 +1,3 @@
+#include "DogEntity.h"
+
+USING_NS_CC;
---
+++ Classes/DogEntity.h
@@ -0,0 +1,12 @@
+#ifndef DOGENTITY_H_
+#define DOGENTITY_H_
+
+#include "cocos2d.h"
+
+class DogEntity : public Entity {
+public:
+
+	CREATE_FUNC(DogEntity);
+};
+
+#endif
